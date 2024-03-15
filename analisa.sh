ee87a66679daa0ccb3748eddb827eb0fa507976a
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-03-15 09:11:49 -0300

    adicionei codigo para extrair colunas do CSV

--- analisa.sh
+++ analisa.sh
@@ -2,4 +2,8 @@
 
 x=$(sed "s/BRA/BR/g" $1 | grep -c "BR");
 
-echo "O arquivo $1 contém $x linhas com a sigla BRA"
+echo "O arquivo $1 contém $x linhas com a sigla BRA";
+
+echo "Os seguintes indicadores estão disponíveis";
+
+echo $(grep BRA dados.csv |cut -d, -s -f1,4,5 )
